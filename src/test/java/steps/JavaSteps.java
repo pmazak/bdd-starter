3171af5e42810db0014805c0b140302f6374534c
Paul <pmazak2002@yahoo.com>
2011-08-04 20:36:09 -0400

    merge

--- src/test/java/steps/JavaSteps.java
+++ src/test/java/steps/JavaSteps.java
@@ -13,20 +13,20 @@ import cuke4duke.annotation.I18n.EN.When;
 
 public class JavaSteps 
 {
-	@Given("^I am learning Cuke$")
-	public void learning() {
-	}
-	
-	@When("^I want to see docs$")
-	public void seeDocumentation() {
-	}
-	
-	@Then("^the site is up$")
-	public void siteAvailable() throws MalformedURLException, IOException
-	{
+    @Given("^I am learning Cuke$")
+    public void learning() {
+    }
+    
+    @When("^I want to see docs$")
+    public void seeDocumentation() {
+    }
+    
+    @Then("^the site is up$")
+    public void siteAvailable() throws MalformedURLException, IOException
+    {
         URLConnection connection = new URL("http://cukes.info/").openConnection();
         connection.connect();
-   	    HttpURLConnection httpConnection = (HttpURLConnection) connection;
+           HttpURLConnection httpConnection = (HttpURLConnection) connection;
         assertEquals(200, httpConnection.getResponseCode());
-	}
+    }
 }
